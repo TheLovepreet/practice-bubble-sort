0049168371162f830f28925917390dd96530f472
TheLove <lovepreetsinghcad@gmail.com>
2022-12-19 20:52:55 -0500

    Bubble sort learned and test passed

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,12 +2,26 @@
 function bubbleSort(arr) {
 
     // Iterate through the array
+    let swaps = 0;
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] > arr[i+1]){
+        [arr[i+1], arr[i]] = [arr[i],arr[i+1]];
+        
+        console.log(arr.join(","));
+        swaps++;
+      }
+    }
+    if(swaps == 0){
+      return;
+    }
+    else{
+      bubbleSort(arr);
+    }
 
       // If the current value is greater than its neighbor to the right
         // Swap those values
 
         // Do not move this console.log
-        console.log(arr.join(","));
 
     // If you get to the end of the array and no swaps have occurred, return
 
