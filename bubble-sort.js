81aa8aa3a791ff8e1e5e7130c4a1ba37d451428c
TheLove <lovepreetsinghcad@gmail.com>
2022-12-20 16:16:25 -0500

    final commit

--- bubble-sort.js
+++ bubble-sort.js
@@ -6,7 +6,6 @@ function bubbleSort(arr) {
     for(let i = 0; i < arr.length; i++){
       if(arr[i] > arr[i+1]){
         [arr[i+1], arr[i]] = [arr[i],arr[i+1]];
-        
         console.log(arr.join(","));
         swaps++;
       }
